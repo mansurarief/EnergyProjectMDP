99f5cb7b7d496ef08f13bc54e68765e7b5c8c8ae
riyakinnarkar <riyakinnarkar@gmail.com>
2025-07-02 23:03:14 +0000

    Put your message

--- examples/main.jl
+++ examples/main.jl
@@ -58,9 +58,19 @@ function calc_nre_percentage(s::State)
     return total_nres/(total_demands + eps)
 end
 
+function calc_re_percentage(s::State)
+    eps = 0.000001
+    total_demands = sum([city.demand for city in s.cities])
+    total_re = sum([city.re_supply for city in s.cities])
+    return total_re/(total_demands + eps)
+end
+
 
 s0 = State(b=10e6, total_demand=0.0, cities = [atlanta, stanford])
 
 
 calc_nre_percentage(s0)
 
+
+using Pkg; Pkg.add("Plots")
+using Plots; plot([1,2,3,4,5], [10,20,15,25,18])
